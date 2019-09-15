a8da1f366e740c9c75514ddd34662119d8bd9f1b
dvarubla <dvarubla@users.noreply.github.com>
2019-09-15 20:53:55 +0700

    Вывод дат, как строк

--- src/main/java/ru/aborisov/testtask/config/AppInitializer.java
+++ src/main/java/ru/aborisov/testtask/config/AppInitializer.java
@@ -1,11 +1,15 @@
 package ru.aborisov.testtask.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.MapPropertySource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
@@ -19,6 +23,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 import java.util.HashMap;
+import java.util.List;
 
 /*Главный класс инициализации*/
 @SuppressWarnings("SpringFacetCodeInspection")
@@ -36,7 +41,21 @@ public class AppInitializer extends AbstractSecurityWebApplicationInitializer im
 
     @Bean
     public ObjectMapper myObjectMapper() {
-        return new ObjectMapper().registerModule(new Jdk8Module());
+        return new ObjectMapper().registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .registerModule(new Jdk8Module());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(myObjectMapper());
+        return mappingJackson2HttpMessageConverter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
     }
 
     @Bean
