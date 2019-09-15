c4bce8350e03ceaab84df22faf6b28ca2f9bb549
dvarubla <dvarubla@users.noreply.github.com>
2019-09-15 13:49:45 +0700

    Поддержка статических страниц

--- src/main/java/ru/aborisov/testtask/config/AppInitializer.java
+++ src/main/java/ru/aborisov/testtask/config/AppInitializer.java
@@ -13,6 +13,8 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
@@ -23,7 +25,14 @@ import java.util.HashMap;
 @Configuration
 @EnableWebMvc
 @ComponentScan("ru.aborisov.testtask")
-public class AppInitializer extends AbstractSecurityWebApplicationInitializer {
+public class AppInitializer extends AbstractSecurityWebApplicationInitializer implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("file:testtaskstatic/");
+    }
 
     @Bean
     public ObjectMapper myObjectMapper() {
@@ -60,6 +69,6 @@ public class AppInitializer extends AbstractSecurityWebApplicationInitializer {
         ServletRegistration.Dynamic dispatcher = servletContext
                 .addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/api/*");
+        dispatcher.addMapping("/*");
     }
 }
